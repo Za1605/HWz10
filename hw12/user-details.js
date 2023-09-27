5fd2bd4bb564d91e7e4c16e6f925c62468479a95
YuliiaT <115080417+Za1605@users.noreply.github.com>
2023-09-27 23:14:14 +0300

    hw 12-eight commit

---
+++ hw12/user-details.js
@@ -0,0 +1,36 @@
+
+
+const userId = new URL(location.href).searchParams.get('userId');
+
+fetch(`http://jsonplaceholder.typicode.com/users/${userId}`)
+    .then((res) => res.json())
+    .then((user) => {
+        console.log(user)
+        const block = document.getElementsByClassName('wrap')[0];
+        const ul = document.createElement('ul');
+        recursiveBuild(user, ul);
+        block.appendChild(ul);
+    });
+
+function liCreator(key, value, parent) {
+    const li = document.createElement('li');
+    li.innerHTML = `<b>${key}:</b> ${value}`;
+    parent.appendChild(li);
+}
+
+function ulBuilder(key, object, parent) {
+    const li = document.createElement('li');
+    const ul = document.createElement('ul');
+    li.innerHTML = `<b>${key}:</b>`;
+    parent.appendChild(li);
+    li.appendChild(ul);
+    recursiveBuild(object, ul);
+}
+
+function recursiveBuild(object, parent) {
+    for (const key in object) {
+        typeof object[key] === 'object'
+            ? ulBuilder(key, object[key], parent)
+            : liCreator(key, object[key], parent)
+    }
+}
